9fb22452ba43f634ad7cd3ffd0bac154fbce66be
duonglhh1 <duonglhh1@vnpay.vn>
2024-07-31 10:39:00 +0700

    feat: first implement of lexer and token

---
+++ src/lexer.rs
@@ -0,0 +1,122 @@
+use anyhow::{bail, Result};
+
+use crate::token::Token;
+
+#[derive(Debug)]
+pub struct Lexer {
+    input: Vec<u8>,
+    position: usize,
+    read_position: usize,
+    ch: u8,
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Self {
+        let mut l = Self {
+            position: 0,
+            read_position: 0,
+            ch: 0,
+            input: input.into_bytes(),
+        };
+        l.read_char();
+
+        l
+    }
+
+    pub fn next_token(&mut self) -> Result<Token> {
+        let token = match self.ch {
+            b'{' => Token::Lbrace,
+            b'}' => Token::Rbrace,
+            b'(' => Token::Lparen,
+            b')' => Token::Rparen,
+
+            b',' => Token::Comma,
+            b';' => Token::Semicolon,
+
+            b'+' => Token::Plus,
+            /* b'-' => Token::Dash,
+            b'!' => {
+                if self.peek() == b'=' {
+                    self.read_char();
+                    Token::NotEqual
+                } else {
+                    Token::Bang
+                }
+            },
+            b'>' => Token::GreaterThan,
+            b'<' => Token::LessThan,
+            b'*' => Token::Asterisk,
+            b'/' => Token::ForwardSlash, */
+            b'=' => {
+                /* if self.peek() == b'=' {
+                    self.read_char();
+                    Token::Equal
+                } else { */
+                    Token::Assign
+                // }
+            },
+            /* b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
+                let ident = self.read_ident();
+                return Ok(match ident.as_str() {
+                    "fn" => Token::Function,
+                    "let" => Token::Let,
+                    /* "if" => Token::If,
+                    "false" => Token::False,
+                    "true" => Token::True,
+                    "return" => Token::Return,
+                    "else" => Token::Else, */
+                    _ => Token::Ident(ident),
+                });
+            },
+            b'0'..=b'9' => return Ok(Token::Int(self.read_int())), */
+            0 => Token::Eof,
+            _ => unreachable!("no monkey program should contain these characters and you should feel bad about yourself")
+        };
+
+        self.read_char();
+        Ok(token)
+    }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = 0;
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use anyhow::Result;
+
+    use super::{Lexer, Token};
+
+    #[test]
+    fn get_next_token() -> Result<()> {
+        let input = "=+(){},;";
+        let mut lexer = Lexer::new(input.into());
+
+        let tokens = vec![
+            Token::Assign,
+            Token::Plus,
+            Token::Lparen,
+            Token::Rparen,
+            Token::Lbrace,
+            Token::Rbrace,
+            Token::Comma,
+            Token::Semicolon,
+        ];
+
+        for token in tokens {
+            let next_token = lexer.next_token()?;
+            println!("expected: {:?}, received {:?}", token, next_token);
+            assert_eq!(token, next_token);
+        }
+
+        return Ok(());
+    }
+}
---
+++ src/token.rs
@@ -0,0 +1,60 @@
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Ident(String),
+    Int(String),
+
+    Illegal,
+    Eof,
+
+    Assign,
+    Plus,
+
+    Comma,
+    Semicolon,
+
+    Lparen,
+    Rparen,
+    Lbrace,
+    Rbrace,
+
+    Function,
+    Let,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return match self {
+            Token::Ident(x) => write!(f, "Ident({})", x),
+            Token::Int(x) => write!(f, "Int({})", x),
+            Token::Illegal => write!(f, "Illegal"),
+            Token::Eof => write!(f, "Eof"),
+            Token::Assign => write!(f, "Assign"),
+            /* Token::Bang => write!(f, "Bang"),
+            Token::Dash => write!(f, "Dash"),
+            Token::ForwardSlash => write!(f, "ForwardSlash"),
+            Token::Asterisk => write!(f, "Asterisk"),
+            Token::Equal => write!(f, "Equal"),
+            Token::NotEqual => write!(f, "NotEqual"),
+            Token::LessThan => write!(f, "LessThan"),
+            Token::GreaterThan => write!(f, "GreaterThan"), */
+            Token::Plus => write!(f, "Plus"),
+            Token::Comma => write!(f, "Comma"),
+            Token::Semicolon => write!(f, "Semicolon"),
+            Token::Lparen => write!(f, "Lparen"),
+            Token::Rparen => write!(f, "Rparen"),
+            Token::Lbrace => write!(f, "Lbrace"),
+            Token::Rbrace => write!(f, "Rbrace"),
+            /* Token::LSquirly => write!(f, "LSquirly"),
+            Token::RSquirly => write!(f, "RSquirly"), */
+            Token::Function => write!(f, "Function"),
+            Token::Let => write!(f, "Let"),
+            /* Token::If => write!(f, "If"),
+            Token::Else => write!(f, "Else"),
+            Token::Return => write!(f, "Return"),
+            Token::True => write!(f, "True"),
+            Token::False => write!(f, "False"), */
+        }
+    }
+}
